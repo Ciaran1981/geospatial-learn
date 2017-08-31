1be4b6a8a16a8a5cbd9b601d62cd76cdf064fb96
Ciaran1981 <ciaran.robb@gmail.com>
2017-08-31 12:00:46 +0100

    gen update

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from io import open
 
 descript = "geospatial-learn is a Python module for using scikit-learn and xgb models with geo-spatial data, chiefly raster and vector formats."
 
+
 with open('README.rst', encoding='utf-8') as f:
-long_description = f.read()
+    long_description = f.read()
 
 setup(
     name="geospatial-learn",
     version="0.1",
     packages=find_packages(),
-
+    install_requires=open('requirements.txt').read().splitlines(),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['docutils>=0.3'],
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
@@ -28,7 +28,7 @@ setup(
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Topic :: Scientific/Engineering :: GIS',
-	  'Topic :: Utilities',
+	      'Topic :: Utilities'],
     # metadata for upload to PyPI
     author="Ciaran Robb",
     description=descript,
