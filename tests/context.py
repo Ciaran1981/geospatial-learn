f680bf166b5fd805e64444e4af87468a8492afe6
John <whenigetone@le.ac.uk>
2017-09-05 10:43:08 +0100

    starting to build test suite

---
+++ tests/context.py
@@ -0,0 +1,6 @@
+#According to Hitchhikers Guide to Python, this is how you do context-free tests
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import sample
