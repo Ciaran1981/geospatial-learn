95ef6399e0b587aa369384d53937855d969d3965
Ciaran1981 <ciaran.robb@gmail.com>
2017-09-18 13:12:44 +0100

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
-    'sphinxcontrib.napoleon'
+#    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
