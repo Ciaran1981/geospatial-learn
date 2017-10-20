4a1357b87473425e20bdb1240ee8b485895724d0
John Roberts <jfr10@leicester.ac.uk>
2017-10-20 12:58:39 +0100

    Added sentinelhub to mocklist

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['gdal', 'ogr', 'osr', 'joblib', 'skimage']
+MOCK_MODULES = ['gdal', 'ogr', 'osr', 'joblib', 'skimage', 'sentinelhub']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
