aaaa1974712d6996f12e988148b71be9a86e76bd
Ciaran1981 <ciaran.robb@gmail.com>
2017-10-31 15:06:50 +0000

    Revert "Added conda yml file and more work on tests"
    This reverts commit 89bc33e056c77591345bf3330fa5ea61a9e6b18e.

--- tests/utilities_test.py
+++ tests/utilities_test.py
@@ -1,7 +1,7 @@
-import pytest
-from .context import geospatial_learn
+import unittest
+from context import geospatial_learn
+
+
 from geospatial_learn.utilities import min_bound_rectangle
 
-
-def test_min_bound_rectangle():
-    assert len(min_bound_rectangle([[1, -1], [1, 1], [-1, 1], [-1, -1]])) == 4
+assert min_bound_rectangle([1, -1]) == 1
