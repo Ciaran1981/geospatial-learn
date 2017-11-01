3efc16c4f48f7e72ac5a512aea625c9868fef2ce
John Roberts <jfr10@le.ac.uk>
2017-11-01 16:15:17 +0000

    Implemented a useful temporary geotiff factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,3 +27,60 @@ def geotiff_dir():
     testDataset = None
     yield tempPath
     tempDir.cleanup()
+
+
+@pytest.fixture
+class TestGeotiffManager:
+    """
+    An object that provides methods to create and manipulate a temporary
+    directory that contains a 3-band geotiff, tempTiff.tif
+
+    Attributes
+    ----------
+    path : str
+        The path to the temporary folder
+    image : list[str]
+        A list of paths to images in the temporary folder
+
+
+
+    """
+    def __init__(self):
+        """Creates a randomly named temp folder
+        """
+        tempDir = tempfile.TemporaryDirectory()
+        fileformat = "GTiff"
+        driver = gdal.GetDriverByName(fileformat)
+        metadata = driver.GetMetadata()
+        tempPath = os.path.join(tempDir.name)
+        testDataset = driver.Create(os.path.join(tempDir.name, "tempTiff.tif"),
+                                    xsize=3, ysize=3, bands=3, eType=gdal.GDT_CFloat32)
+        for i in range(3):
+            testDataset.GetRasterBand(i + 1).WriteArray(np.ones([3, 3]))
+
+        self.path = tempPath
+        self.image =[]
+        self.image.append(os.path.join(tempPath, "tempTiff.tif"))
+        self._tempDir = tempDir
+
+    def __enter__(self):
+        # null
+
+    def __exit__(self):
+        self._tempDir.cleanup()
+
+    def create_temp_tiff(self, name, content = None):
+        """Creates a temporary geotiff in self.path
+        """
+        if not content:
+            content = np.zeros([3, 3, 3])
+        path = os.path.join(self.path, name)
+        driver = gdal.GetDriverByName('.tif')
+        driver.Create(
+            path,
+            xsize=content.shape[1],
+            ysize=content.shape[2],
+            bands=content.shape[0],
+            eType=gdal.GTD_CFloat32
+        )
+        self.image.append(path)
--- tests/geodata_test.py
+++ tests/geodata_test.py
@@ -3,7 +3,7 @@ import os
 import gdal
 import numpy as np
 from context import geospatial_learn
-from geospatial_learn.geodata import array2raster
+from geospatial_learn import geodata
 
 
 def test_array2raster(geotiff_dir):
@@ -12,6 +12,12 @@ def test_array2raster(geotiff_dir):
     inRaster = os.path.join(geotiff_dir, "tempTiff.tif")
     outRaster = os.path.join(geotiff_dir, "fromArray")
     dtype = gdal.GDT_Int32
-    array2raster(array, bands, inRaster, outRaster, dtype)
+    geodata.array2raster(array, bands, inRaster, outRaster, dtype)
     result = gdal.Open(outRaster + ".tif").ReadAsArray()
     assert result[0, 1, 2] == 0
+
+
+def test_multi_temp_filter(TestGeotiff):
+    result = geodata.multi_temp_filter(
+        inRas=
+    )
