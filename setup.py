c1d51987cf8dbb80c3b133a9f2783fb20a661e03
Ciaran1981 <ciaran.robb@gmail.com>
2017-12-13 17:24:19 +0000

    demo rot

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name="geospatial-learn",
-    version="0.128",
+    version="0.130",
     packages=['geospatial_learn'],
     install_requires=open('requirements.txt').read().splitlines(),
     # Project uses reStructuredText, so ensure that the docutils get
