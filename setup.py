f389e06f77466bf0bca06a8f723f0d1645aaf00e
John Roberts <jfr10@le.ac.uk>
2017-12-18 11:57:59 +0000

    Extended setup.py to install conda modules after the regular one

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 # -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
+"""Alternations to setup.py based on Brandon Rhodes' conda setup.py:
+https://github.com/brandon-rhodes/conda-install"""
+from setuptools import setup
+from setuptools.command.install import install
 from io import open
-
+import subprocess
 
 descript = ('geospatial-learn is a Python module for using scikit-learn and'
             'xgb models with geo-spatial data, chiefly raster and vector'
             'formats.')
 
-
 with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
+
+class CondaInstall(install):
+    def run(self):
+        try:
+            install.run(self)
+            command = ['conda', 'install']
+            packages = open('conda_modules.txt').read().splitlines()
+            command.extend(packages)
+            subprocess.check_call(command)
+        except subprocess.CalledProcessError:
+            print("Conda install failed: do you have Anaconda/miniconda installed and on your PATH?")
+
+
 setup(
+    cmdclass={'install': CondaInstall},
     name="geospatial-learn",
-    version="0.128",
+    version="0.2.dev1",
     packages=['geospatial_learn'],
     install_requires=open('requirements.txt').read().splitlines(),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    include_package_data= True,#{
+    include_package_data=True,# {
         # If any package contains *.txt or *.rst files, include them:
         # And include any *.msg files found in the 'hello' package, too:
     #},
@@ -29,10 +44,10 @@ setup(
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Topic :: Scientific/Engineering :: GIS',
-	      'Topic :: Utilities'],
+          'Topic :: Utilities'],
     # metadata for upload to PyPI
-    #zip_safe = True,
-    author="Ciaran Robb",
+    # zip_safe = True,
+    author="Ciaran Robb and John Roberts",
     description=descript,
     long_description=long_description,
     license='GPLv3+',
